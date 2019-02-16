15987686b1fbade195de8e74c9f0e5f810cbaffc
Miri <mirivhbv@gmail.com>
2019-02-16 11:41:33 +0400

    Initial commit

---
+++ SimpleGet/Controllers/HomeControllercs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleGet.Controllers
+{
+    [ApiController]
+    public class HomeControllercs
+    {
+        [Route("")]
+        public async Task<IActionResult> Index()
+        {
+            return new OkResult();
+        }
+
+        [Route("/farid")]
+        [HttpGet]
+        public async Task<IActionResult> Farid()
+        {
+            return new OkResult();
+        }
+    }
+}
